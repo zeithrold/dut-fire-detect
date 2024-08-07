a06883a69b463733b0bfd3798c62d033371a4d76
zeithrold <zeithrold@gmail.com>
2024-08-07 23:53:00 +0800

    feat: added installed_providers to display

--- dut_fire_detect/model.py
+++ dut_fire_detect/model.py
@@ -6,16 +6,18 @@ import onnxruntime as ort
 from loguru import logger
 
 all_available_providers: List[str] = ort.get_available_providers()
+installed_providers = ort.get_all_providers()
+logger.info(f"Installed providers: {installed_providers}")
 if "AzureExecutionProvider" in all_available_providers:
     all_available_providers.remove("AzureExecutionProvider")
 
 if all_available_providers == ["CPUExecutionProvider"]:
     logger.warning(
         "Only CPUExecutionProvider is available, "
-        "which means the model will run only on CPU."
-        "Tt's either because you don't have a GPU or"
-        "your GPU don't support corresponding providers"
-        "(no CUDA Toolkit for CUDA version, or no DirectX 12 for DirectML version)."
+        "which means the model will run only on CPU. "
+        "It's either because you don't have a GPU or "
+        "your GPU don't support corresponding providers "
+        "(no CUDA Toolkit for CUDA version, or no DirectX 12 for DirectML version). "
         "You should consider installing other providers for better performance."
     )
 
